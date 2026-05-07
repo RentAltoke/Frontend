c23cbc2853deed5f5e767b0d9c0ea53a3db38f6d
Jamir Rojas <jamroall89@gmail.com>
2026-05-07 01:38:31 -0500

    Navbar y Sidebar nuevos

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../features/auth/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css'],
 })
 export class Sidebar {
+  collapsed = false;
+
   constructor(private router: Router, private authService: AuthService) {}
+
+  toggleSidebar() {
+    this.collapsed = !this.collapsed;
+  }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/inicio']);
   }
+  
 }
+
