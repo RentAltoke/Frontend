bb1ff551ed70d2b1a0eb7fec585a2b50210be004
Jamir Rojas <jamroall89@gmail.com>
2026-05-07 13:20:34 -0500

    Funcionalidad de la pagina Inicio

--- src/app/features/inicio/inicio.ts
+++ src/app/features/inicio/inicio.ts
@@ -1,33 +1,26 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import { Router } from '@angular/router'; // Asegúrate de tener el router
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-inicio',
+  standalone: true,
   imports: [],
   templateUrl: './inicio.html',
-  styleUrl: './inicio.css',
+  styleUrls: ['./inicio.css'],
 })
 export class Inicio {
-role: string = 'SECRETARIO ADMINISTRADOR';
-constructor(
+  role: string = 'SECRETARIO ADMINISTRADOR';
+
+  constructor(
     private cdr: ChangeDetectorRef,
     private router: Router
   ) {}
 
-  // Esta es la función que debes llamar desde el (click) en tu HTML
   navegarAModulo(ruta: string) {
     this.router.navigate([ruta]).then((nav) => {
-      if(nav) {
-        console.log('Navegación exitosa');
-      } else {
-        // Si la navegación falla o se queda "pegada", forzamos el chequeo
+      if(!nav) {
         this.cdr.detectChanges();
       }
-    }).catch(err => {
-      console.error(err);
-      this.cdr.detectChanges();
     });
-
-    // TIP EXTRA: A veces, envolverlo en un microtask ayuda a sincronizar la UI
-    this.cdr.detectChanges(); 
   }
 }
