5f8d46d0553babb065bc8c029069574b713429be
Jamir Rojas <jamroall89@gmail.com>
2026-06-08 19:51:45 -0500

    Arreglo Test Inmuebles

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
+import { provideLocationMocks } from '@angular/common/testing';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideRouter([]),
+        provideLocationMocks(),
+      ]
     }).compileComponents();
   });
 
@@ -14,16 +20,12 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-it('should render title', async () => {
-  const fixture = TestBed.createComponent(App);
+  it('should render title', async () => {
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+    await fixture.whenStable();
 
-  fixture.detectChanges();
-  await fixture.whenStable();
-
-  const compiled = fixture.nativeElement as HTMLElement;
-
-  expect(compiled.textContent).toContain('Hello');
-});
-
-
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
 });
--- src/app/features/inmuebles/inmueble-list/inmueble-list.spec.ts
+++ src/app/features/inmuebles/inmueble-list/inmueble-list.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InmuebleList } from './inmueble-list';
+import { provideRouter } from '@angular/router';
+import { provideLocationMocks } from '@angular/common/testing';
+import { InmuebleService } from '../inmueble.service';
+import { of } from 'rxjs';
+
+// Mock del servicio para no hacer llamadas reales al backend
+const inmuebleServiceMock = {
+  getAll: () => of([]) // retorna un observable vacío
+};
 
 describe('InmuebleList', () => {
   let component: InmuebleList;
@@ -9,6 +17,11 @@ describe('InmuebleList', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [InmuebleList],
+      providers: [
+        provideRouter([]),
+        provideLocationMocks(),
+        { provide: InmuebleService, useValue: inmuebleServiceMock }, // 👈 reemplaza el servicio real
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(InmuebleList);
