12d414253bb70707da8446324d8f63bd62cff2fe
Alex Acosta <alexanderx100x@gmail.com>
2026-06-14 17:29:31 -0500

    cambios vercel3

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -22,6 +22,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'unidades/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
