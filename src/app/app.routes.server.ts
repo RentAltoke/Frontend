f1d9d839516597da4d9a6f1f2cbf756b363294fe
Alex Acosta <alexanderx100x@gmail.com>
2026-06-14 17:20:07 -0500

    cambios vercel1

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,22 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+{
+    path: 'inmuebles/editar/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'unidades/editar/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'unidades/:id/nueva',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'caja/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
