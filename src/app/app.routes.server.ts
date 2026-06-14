bb91b89c6decc88c5d24b43181d8a2c58e984ae1
Alex Acosta <alexanderx100x@gmail.com>
2026-06-14 17:26:00 -0500

    vercel cambio2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -18,6 +18,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'unidades/nuevo/:inmuebleId',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
